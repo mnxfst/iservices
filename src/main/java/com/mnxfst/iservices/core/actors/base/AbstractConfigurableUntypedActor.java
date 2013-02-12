aeface8755b3e333e410632f1c5cd19c73f928fa
mnxfst <mnxfst@gmail.com>
2013-02-12 23:37:23 +0100

    continued

--- src/main/java/com/mnxfst/iservices/core/actors/base/AbstractConfigurableUntypedActor.java
+++ src/main/java/com/mnxfst/iservices/core/actors/base/AbstractConfigurableUntypedActor.java
@@ -3,6 +3,16 @@
  */
 package com.mnxfst.iservices.core.actors.base;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
 import akka.actor.UntypedActor;
 
 /**
@@ -13,6 +23,20 @@ import akka.actor.UntypedActor;
  */
 public abstract class AbstractConfigurableUntypedActor extends UntypedActor {
 
-		
+	/**
+	 * Parses the provided configuration file into a {@link Document} representation 
+	 * @param configuration
+	 * @return
+	 * @throws SAXException
+	 * @throws IOException
+	 * @throws ParserConfigurationException
+	 */
+	protected Document parseConfiguration(String configuration) throws SAXException, IOException, ParserConfigurationException {		
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		factory.setNamespaceAware(false);
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		Document document = builder.parse(new ByteArrayInputStream(configuration.getBytes()));
+		return document;		
+	}
 	
 }
