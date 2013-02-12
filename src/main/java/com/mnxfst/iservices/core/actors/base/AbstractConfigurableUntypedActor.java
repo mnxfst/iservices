64fa7223834293f0491eb7a8aada9aef1fa482fc
mnxfst <mnxfst@gmail.com>
2013-02-12 23:26:53 +0100

    initial

---
+++ src/main/java/com/mnxfst/iservices/core/actors/base/AbstractConfigurableUntypedActor.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.mnxfst.iservices.core.actors.base;
+
+import akka.actor.UntypedActor;
+
+/**
+ * Provides a parent to all actors that wish to utilize the implemented convenience
+ * methods for configuring actor instances
+ * @author ckreutzfeldt
+ *
+ */
+public abstract class AbstractConfigurableUntypedActor extends UntypedActor {
+
+		
+	
+}
